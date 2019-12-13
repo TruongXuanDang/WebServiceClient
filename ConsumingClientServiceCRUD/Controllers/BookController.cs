bcf83249ef2111a259b58b123f559045fe29b757
TruongDX8 <truonghusk17@gmail.com>
2019-12-13 23:55:56 +0700

    first commit

---
+++ ConsumingClientServiceCRUD/Controllers/BookController.cs
@@ -0,0 +1,47 @@
+﻿using ConsumingClientServiceCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConsumingClientServiceCRUD.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        ServiceReference5.Book b = new ServiceReference5.Book();
+        ServiceReference5.BookServicesClient mbsClient = new ServiceReference5.BookServicesClient();
+        public ActionResult Index()
+        {
+            ViewBag.listBooks = mbsClient.GetBookList();
+            return View();
+
+        }
+
+        public ActionResult Create(string title, string isbn)
+        {
+            mbsClient.AddBook(new ServiceReference5.Book() { Title = title, ISBN = isbn });
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Update(string title, string isbn, int bookId)
+        {
+            mbsClient.UpdateBook(new ServiceReference5.Book() { Title = title, ISBN = isbn, BookId = bookId },bookId.ToString());
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Detail(int bookId)
+        {
+            var book = mbsClient.GetBookById(bookId.ToString());
+            ViewBag.book = book;
+            return View();
+        }
+
+        public ActionResult Delete(int bookId)
+        {
+            mbsClient.DeleteBook(bookId.ToString());
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ConsumingClientServiceCRUD/Models/BookServiceClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace ConsumingClientServiceCRUD.Models
+{
+    public class BookServiceClient
+    {
+    string BASE_URL = "http://localhost:60603/BookServices.svc";
+
+        public List<Book> getAllBooks()
+        {
+            var syncClient = new WebClient();
+            var content = syncClient.DownloadString(BASE_URL + "/Book");
+            var json_serializer = new JavaScriptSerializer();
+            return json_serializer.Deserialize<List<Book>>(content);
+        }
+    }
+}
